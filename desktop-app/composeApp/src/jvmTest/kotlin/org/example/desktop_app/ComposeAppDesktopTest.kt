f0c7bd5b26ec007e18de63c6f21742a5ce2870bc
Sherif-Moheep <sherifmohep@gmail.com>
2026-02-16 16:35:32 +0200

    finalized the chrome extenstion

---
+++ desktop-app/composeApp/src/jvmTest/kotlin/org/example/desktop_app/ComposeAppDesktopTest.kt
@@ -0,0 +1,12 @@
+package org.example.desktop_app
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppDesktopTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
